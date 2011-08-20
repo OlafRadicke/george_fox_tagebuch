471cf4706aa8789ee85afecb6071f5a0544d2080
Olaf Radicke <briefkasten@olaf-radicke.de>
2011-08-20 18:44:42 +0200

    Neue Verzeichnisse im Index.

--- make_pdf.sh
+++ make_pdf.sh
@@ -2,5 +2,22 @@
 
 pdflatex komplett.tex
 makeindex komplett.idx
+# Index "brief"
+makeindex komplett.brdx -o komplett.brnd
+# Index "ort"
+makeindex komplett.odx -o komplett.ond
+# Index "person"
+makeindex komplett.pdx -o komplett.pnd
+
+# 2
 pdflatex komplett.tex
+makeindex komplett.idx
+# Index "brief"
+makeindex komplett.brdx -o komplett.brnd
+# Index "ort"
+makeindex komplett.odx -o komplett.ond
+# Index "person"
+makeindex komplett.pdx -o komplett.pnd
+
+# 3
 pdflatex komplett.tex
